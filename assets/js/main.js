38131a424b63978651329c1d6b280c4d57cb5b0f
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2022-08-23 11:54:37 -0300

    create carousel filmes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 import header from './modules/header.js'
 import bannerSlider from './modules/banner-slider.js'
+import collections from './modules/collections.js'
 
 
-bannerSlider.init()
 header.init()
+bannerSlider.init()
+collections.init()
